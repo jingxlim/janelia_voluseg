4b62c1fb56c2573c71f7dbd0d03127f253cfc6b8
Lim Jing Xuan <jingx.lim@outlook.com>
2021-07-04 03:10:23 -0400

    updated spark_properties_ws1.conf so that we might run pipeline on ws3

--- run_local_voluseg.sh
+++ run_local_voluseg.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 source ~/janelia_voluseg/.bashrc_voluseg
 mkdir $SPARK_LOG_DIR
-dir_output=/scratch/limj2/data/20210603/fish00/4dpf_HuC-h2b-GC7F_MG-vs-NGGU-bright-to-loom_fish00_exp02_20210603_180846/im_CM0_voluseg
+dir_output=/scratch/limj2/data/20201017/fish02/8dpf_HuC-GCaMP7f-Gfap-rgeco_barcoding_fish02_exp04_20201017_224356/im_CM0_voluseg
 output_log_path="${dir_output}/$(date +%Y%m%d_%H%M%S).log"
 properties_file_path=~/janelia_voluseg/spark_properties_ws1.conf
 echo "output log: ${output_log_path}"
-nohup $SPARK_HOME/bin/spark-submit --properties-file $properties_file_path ~/janelia_voluseg/voluseg_submit.py $dir_output >> $output_log_path&
+nohup $SPARK_HOME/bin/spark-submit --master local[*] --properties-file $properties_file_path ~/janelia_voluseg/voluseg_submit.py $dir_output >> $output_log_path&
